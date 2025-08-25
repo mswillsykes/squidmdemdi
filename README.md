4c030fd71cfaed9f40b437f5c39b27f63d2b8fe2
mswillsykes <willsykes@microsoft.com>
2025-08-25 10:23:52 -0700

    Updated script with new endpoints and performance tweaks learned from field. Updated README with new instructions and revised guidance

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # squidmdemdi
 
-## This isn't a Microsoft produced script, this is my creation.
+# This isn't a Microsoft produced script, this is my creation.
 
-The proxy in a box is designed to run on Ubuntu Linux and is a preconfigured, known good Squid install with only the MDE/MDI endpoints allowed in the mdemdi.conf file. The squid.conf file is configured to only allow connections from RFC 1918 addresses, so public address space cannot be used to connect to the proxy. If your environment is using public address space internally you'll need to modify the squid.conf file.
+# Why to use
+This solution is designed to be used to deployment. Often systems cannot be allowed direct internet access or there are other filtering solutions in place for the system to use. This proxy is designed to be configured at the application level, so that the operating system can utilize its own proxy or internet rules, where MDE/MDI can use this proxy to talk to their required endpoints.
+
+# About
+The proxy in a box is designed to run on Ubuntu Linux and is a preconfigured, known good Squid install with only the MDE/MDI endpoints allowed in the mdemdi.conf file. The squid.conf file is configured to only allow connections _from_ RFC 1918 addresses, so public address space cannot be used to connect to the proxy. If your environment is using public address space internally you'll need to modify the squid.conf file.
 
 The squid.conf file only has 1 allow list of endpoints configured at /etc/squid/mdemdi.conf. This proxy is specifically designed not for system use, but only to be used by the MDE/MDI applications. However, you can still utilize this for generic use as long as you provision the endpoints. If you need to allow additional endpoints the easiest way is to edit the /etc/squid/mdemdi.conf file. If you want you can add your own allow list file and edit the squid.conf file to incorporate your new endpoint file.
 
-Create an Ubuntu server (anything more than 18.04). Nothing special needs to be in the install.
+# Instructions
+Create an Ubuntu server (anything supported). Nothing special needs to be in the install.
 
-Login and run the build script. The build script must be run as root (sudo -i) or sudo.
+Login and run the SquidConfig.sh script. The script must be run as root (sudo -i) or sudo.
--- SquidConfig.sh
+++ SquidConfig.sh
@@ -9,118 +9,34 @@ echo y | ufw enable
 mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
 
 cat <<EOF >/etc/squid/mdemdi.conf
-crl.microsoft.com
-www.microsoft.com
-events.data.microsoft.com
-.notify.windows.com
-.wns.windows.com
-login.microsoftonline.com
-login.live.com
-settings-win.data.microsoft.com
-officecdn-microsoft-com.akamaized.net
-crl.microsoft.com
-packages.microsoft.com
-enterpriseregistration.windows.net 
-login.microsoftonline.com
-.dm.microsoft.com 
-.ods.opinsights.azure.com
-.oms.opinsights.azure.com
-.azure-automation.net
-.blob.core.windows.net
-login.windows.net  
-.securitycenter.windows.com 
-us.vortex-win.data.microsoft.com
-us-v20.events.data.microsoft.com
-winatp-gw-cus.microsoft.com
-winatp-gw-eus.microsoft.com
-winatp-gw-cus3.microsoft.com
-winatp-gw-eus3.microsoft.com
-automatedirstrprdcus.blob.core.windows.net
-automatedirstrprdeus.blob.core.windows.net
-automatedirstrprdcus3.blob.core.windows.net
-automatedirstrprdeus3.blob.core.windows.net
-ussus1eastprod.blob.core.windows.net
-ussus2eastprod.blob.core.windows.net
-ussus3eastprod.blob.core.windows.net
-ussus4eastprod.blob.core.windows.net
-wsus1eastprod.blob.core.windows.net
-wsus2eastprod.blob.core.windows.net
-ussus1westprod.blob.core.windows.net
-ussus2westprod.blob.core.windows.net
-ussus3westprod.blob.core.windows.net
-ussus4westprod.blob.core.windows.net
-wsus1westprod.blob.core.windows.net
-wsus2westprod.blob.core.windows.net
-eu.vortex-win.data.microsoft.com
-eu-v20.events.data.microsoft.com
-winatp-gw-neu.microsoft.com
-winatp-gw-weu.microsoft.com
-winatp-gw-neu3.microsoft.com
-winatp-gw-weu3.microsoft.com
-automatedirstrprdneu.blob.core.windows.net
-automatedirstrprdweu.blob.core.windows.net
-automatedirstrprdneu3.blob.core.windows.net
-automatedirstrprdweu3.blob.core.windows.net
-usseu1northprod.blob.core.windows.net
-wseu1northprod.blob.core.windows.net
-usseu1westprod.blob.core.windows.net
-wseu1westprod.blob.core.windows.net
-uk.vortex-win.data.microsoft.com
-uk-v20.events.data.microsoft.com
-winatp-gw-uks.microsoft.com
-winatp-gw-ukw.microsoft.com
-automatedirstrprduks.blob.core.windows.net
-automatedirstrprdukw.blob.core.windows.net
-ussuk1southprod.blob.core.windows.net
-wsuk1southprod.blob.core.windows.net
-ussuk1westprod.blob.core.windows.net
-wsuk1westprod.blob.core.windows.net
-.update.microsoft.com
-.delivery.mp.microsoft.com
-.windowsupdate.com
-go.microsoft.com 
-definitionupdates.microsoft.com 
-.download.windowsupdate.com
-.download.microsoft.com
-fe3cr.delivery.mp.microsoft.com
-crl.microsoft.com
-vortex-win.data.microsoft.com
-settings-win.data.microsoft.com
-.wdcp.microsoft.com
-.wdcpalt.microsoft.com
-.wd.microsoft.com
-ussus1eastprod.blob.core.windows.net
-ussus2eastprod.blob.core.windows.net
-ussus3eastprod.blob.core.windows.net
-ussus4eastprod.blob.core.windows.net
-wsus1eastprod.blob.core.windows.net
-wsus2eastprod.blob.core.windows.net
-ussus1westprod.blob.core.windows.net
-ussus2westprod.blob.core.windows.net
-ussus3westprod.blob.core.windows.net
-ussus4westprod.blob.core.windows.net
-wsus1westprod.blob.core.windows.net
-wsus2westprod.blob.core.windows.net
-usseu1northprod.blob.core.windows.net
-wseu1northprod.blob.core.windows.net
-usseu1westprod.blob.core.windows.net
-wseu1westprod.blob.core.windows.net
-ussuk1southprod.blob.core.windows.net
-wsuk1southprod.blob.core.windows.net
-ussuk1westprod.blob.core.windows.net
-wsuk1westprod.blob.core.windows.net
-.smartscreen-prod.microsoft.com
-.smartscreen.microsoft.com
-.checkappexec.microsoft.com
-.urs.microsoft.com
-.ods.opinsights.azure.com
-.oms.opinsights.azure.com
-.blob.core.windows.net
-.azure-automation.net
-azure.archive.ubuntu.com
-packages.microsoft.com
-winatp-gw-cane.microsoft.com
 .atp.azure.com
+.azure-automation.net
+.azurewebsites.net
+.blob.core.usgovcloudapi.net
+.blob.core.windows.net
+.cloud.microsoft
+.digicert.com
+.entrust.net
+.microsoft.com
+.microsoft365.com
+.microsoftonline-p.com
+.ods.opinsights.azure.com
+.ods.opinsights.azure.us
+.oms.opinsights.azure.com
+.oms.opinsights.azure.us
+.securitycenter.microsoft.us
+.ss.wd.microsoft.us
+.ssllabs.com
+.windows.com
+.windowsupdate.com
+azure.archive.ubuntu.com
+enterpriseregistration.windows.net
+login.live.com
+login.microsoftonline.com
+login.microsoftonline.us
+login.windows.net
+officecdn-microsoft-com.akamaized.net
+static2.sharepointonline.com
 EOF
 
 cat <<EOF >/etc/squid/squid.conf
@@ -145,4 +61,17 @@ http_access deny CONNECT
 http_access deny all
 EOF
 
+sed -i '/^proxy soft nofile/d' /etc/security/limits.conf
+sed -i '/^proxy hard nofile/d' /etc/security/limits.conf
+sed -i '/^# End of file/i proxy soft nofile 65535' /etc/security/limits.conf
+sed -i '/^proxy soft nofile 65535/i proxy hard nofile 65535' /etc/security/limits.conf
+sed -i '/^session required pam_limits.so/d' /etc/pam.d/common-session
+sed -i '/^session required pam_limits.so/d' /etc/pam.d/common-session-noninteractive
+sed -i '/^# end of pam-auth-update config/i session required pam_limits.so' /etc/pam.d/common-session
+sed -i '/^# end of pam-auth-update config/i session required pam_limits.so' /etc/pam.d/common-session-noninteractive
+sed -i '/^DefaultLimitNOFILE=/d' /etc/systemd/user.conf
+sed -i '/^DefaultLimitNOFILE=/d' /etc/systemd/system.conf
+echo DefaultLimitNOFILE=65535 >> /etc/systemd/user.conf
+echo DefaultLimitNOFILE=65535 >> /etc/systemd/system.conf
+sudo systemctl daemon-reexec
 systemctl restart squid
